aca47970e75630f25efe572424d418a2aba50fd9
Thomas <thomasdriscoll98@gmail.com>
2019-05-14 21:11:44 -0500

    added html/css for top bar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @Component({
-  selector: 'navbar',
+  selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  title = 'open-source@illinois';
 
   constructor() { }
 
