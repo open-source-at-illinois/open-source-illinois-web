34bd79b00ab717d0490d6cb8acbe0dd0d4ad7335
Pranav <pyanda2@illinois.edu>
2019-10-26 20:18:44 -0500

    color change website

---
+++ server/controllers/announcement.controller.js
@@ -0,0 +1,9 @@
+const BaseController = require('./base.controller');
+const mongoose = require('mongoose');
+
+class Announcement extends BaseController {
+  constructor(name) {
+    super(name);
+
+  }
+}
