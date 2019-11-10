6d69d8ae45ae0791bcdc0ba28bc003a0c59da4ef
karywjh <jiahuiw4@illinois.edu>
2019-11-09 23:56:13 -0600

    Added Announcement Button to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _bottomSheet: MatBottomSheet) { }
 
   ngOnInit() {
   }
 
+  openBottomSheet(): void {
+    this._bottomSheet.open(Announcement);
+  }
+}
+@Component({
+  selector: 'announcement',
+  templateUrl: 'home.announcement.html',
+})
+export class Announcement {
+  constructor(private _bottomSheetRef: MatBottomSheetRef<Announcement>) {}
+
+  openLink(event: MouseEvent): void {
+    this._bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
 }
