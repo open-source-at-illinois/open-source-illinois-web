a3d9c65922c52d7708119a28e00ca35b014e25e5
thomasdriscoll <thomasdriscoll98@gmail.com>
2020-01-25 15:01:12 -0600

    updated production's file location

--- server/config/environment.js
+++ server/config/environment.js
@@ -1,13 +1,15 @@
 const dev_environment = {
     port : 3000,
     frontUrl : 'http://localhost:4200',
-    backUrl: 'http://localhost:3000'
+    backUrl: 'http://localhost:3000',
+    production: false
 }
 
 const prod_environment = {
     port: 3000,
     frontUrl: 'https://www.osai-web.com',
-    backUrl: 'https://www.osai-web.com:3000'
+    backUrl: 'https://www.osai-web.com:3000',
+    production: true
 }
 // Comment out the one you don't want
 // module.exports = dev_environment; 
