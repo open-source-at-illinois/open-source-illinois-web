4b3a40b5a2e3ad75e470470a5487af613f65381e
thomasdriscoll <thomasdriscoll98@gmail.com>
2020-02-06 23:05:32 -0600

    update backUrl (again)

--- server/config/environment.js
+++ server/config/environment.js
@@ -1,14 +1,14 @@
 const dev_environment = {
     port : 3000,
     frontUrl : 'http://localhost:4200',
-    backUrl: 'http://localhost:3000',
+    backUrl: 'http://localhost:3000/',
     production: false
 }
 
 const prod_environment = {
     port: 3000,
     frontUrl: 'https://www.osai-web.com',
-    backUrl: 'https://www.osai-web.com/back-end',
+    backUrl: 'https://www.osai-web.com/back-end/',
     production: false
 }
 // Comment out the one you don't want
