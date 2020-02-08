8c7347a852b580fd9c3899450de1d7e4b51b1220
thomasdriscoll <thomasdriscoll98@gmail.com>
2020-02-08 11:01:43 -0600

    stack trace bby

--- server/app.js
+++ server/app.js
@@ -27,4 +27,17 @@ app.use(checkJwt);
 app.use(bodyParser.json());
 app.use('/back-end/api', routes);
 
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: err
+  });
+});
 module.exports = app;
